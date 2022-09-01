6c65e98e0a10eaa588504ceb012ad879daa3a664
Javaughn Pryce <javanpryce1@gmail.com>
2022-09-01 19:49:20 -0400

    **changes**

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
     <header className="bg-transparent p-5 text-white">
       <div className="mx-auto flex max-w-6xl items-center justify-between">
         <h1 className="z-10 font-serif text-lg font-bold">Karaoke Central</h1>
-        <div className="space-x-4">
+        <div className="z-10 space-x-4">
           {!mobile && (
             <Link
               className="rounded bg-transparent bg-sky-500 px-3 py-2 font-semibold text-white shadow-lg shadow-sky-500/40 transition duration-300 ease-in-out"
